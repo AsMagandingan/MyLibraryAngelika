df80b7dde2ec998bddaa0670097b85577c037c43
AsMagandingan <asmagandingan@gmail.com>
2026-05-19 12:52:25 +0800

    Create story3.php

---
+++ fa4/story3.php
@@ -0,0 +1,14 @@
+<?php require 'header.php'; ?>
+
+<div class="container">
+    <h2>The Magic Book</h2>
+
+    <img src="images/story3.jpg">
+
+    <p>
+        Maria discovered an old magical book in the library. Every page she opened
+        transported her into a different adventure full of mystery and excitement.
+    </p>
+</div>
+
+<?php include 'footer.php'; ?>
