ceba736e8d43d169b164d3a5a05d7484a1634112
AsMagandingan <asmagandingan@gmail.com>
2026-05-19 15:25:33 +0800

    Create lei.php

---
+++ fa4/act1/lei.php
@@ -0,0 +1,14 @@
+<div class="card">
+
+    <h1><b>LEI</b></h1>
+
+    <img src="images/lei.jpg">
+
+    <<p><b>Zodiac Sign:</b>Taurus</p>
+
+    <p>
+        My Friend with the same craziness as mine,
+        We have each others back.
+    </p>
+
+</div>
