6ff3cef7a582b0546ead579b54d907c06e7c5136
AsMagandingan <asmagandingan@gmail.com>
2026-05-19 13:12:36 +0800

    Create story2.php

---
+++ fa4/act1/story2.php
@@ -0,0 +1,14 @@
+<?php require 'header.php'; ?>
+
+<div class="container">
+    <h2>The Lost Puppy</h2>
+
+    <img src="images/story2.jpg">
+
+    <p>
+        A small puppy got lost during a rainy evening. A young boy found the puppy
+        and helped it return home safely. The puppy’s owner was very thankful.
+    </p>
+</div>
+
+<?php include 'footer.php'; ?>
