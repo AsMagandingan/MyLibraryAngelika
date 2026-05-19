a401bda86f09c6b18ecdcbce5c1c7c94eb08157e
AsMagandingan <asmagandingan@gmail.com>
2026-05-19 15:26:31 +0800

    Create ashlee.php

---
+++ fa4/act1/ashlee.php
@@ -0,0 +1,14 @@
+<div class="card">
+
+    <h1><b>ASHLEE</b></h1>
+
+    <img src="images/ashlee.jpg">
+
+    <p><b>Zodiac Sign:</b>Libra</p>
+
+    <p>
+        My little cousin/sister who is always been there for me,
+        who keeps me company and makes me laugh, and who I love so much.
+    </p>
+
+</div>
