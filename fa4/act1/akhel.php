10e6ffa38b2d42346b984b819505f0800d22016e
AsMagandingan <asmagandingan@gmail.com>
2026-05-19 15:26:34 +0800

    Create akhel.php

---
+++ fa4/act1/akhel.php
@@ -0,0 +1,14 @@
+<div class="card">
+
+    <h1><b>AKHEL</b></h1>
+
+    <img src="images/akhel.jpg">
+
+    <p><b>Zodiac Sign:</b>Taurus</p>
+
+    <p>
+        My little brother, who is always there for me,
+         no matter what. We have a strong bond and I cherish our relationship.
+    </p>
+
+</div>
