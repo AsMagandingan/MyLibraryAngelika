bff3595279e55f54aa28eb9b4c89d176f88949e7
AsMagandingan <asmagandingan@gmail.com>
2026-05-19 15:24:53 +0800

    Create taylorswift.php

---
+++ fa4/act1/taylorswift.php
@@ -0,0 +1,14 @@
+<div class="card">
+
+    <h1><b>TAYLOR</b></h1>
+
+    <img src="images/taylorswift.jpg">
+
+    <p><b>Zodiac Sign:</b>Sagitarrius</p>
+
+    <p>
+       Ever sign I can remember, 
+       her music has always been there for me.
+    </p>
+
+</div>
