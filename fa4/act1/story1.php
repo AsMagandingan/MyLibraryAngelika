fb4e71d469369b326d3952eeaa5803cdcf00f529
AsMagandingan <asmagandingan@gmail.com>
2026-05-19 13:12:30 +0800

    Create story1.php

---
+++ fa4/act1/story1.php
@@ -0,0 +1,15 @@
+<?php require 'header.php'; ?>
+
+<div class="container">
+    <h2>The Brave Student</h2>
+
+    <img src="images/story1.jpg">
+
+    <p>
+        Anna was a hardworking student who dreamed of becoming a programmer.
+        Despite many challenges, she continued studying every night and finally
+        achieved her dream of becoming a successful web developer.
+    </p>
+</div>
+
+<?php include 'footer.php'; ?> 
