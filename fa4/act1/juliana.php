dc93451ab80d17eacef7d088cd81ec7e6af3c701
AsMagandingan <asmagandingan@gmail.com>
2026-05-19 15:25:43 +0800

    Create juliana.php

---
+++ fa4/act1/juliana.php
@@ -0,0 +1,14 @@
+<div class="card">
+
+    <h1><b>JULIANA</b></h1>
+
+    <img src="images/juliana.jpg">
+
+    <p><b>Zodiac Sign:</b>Virgo</p>
+
+    <p>
+       Since the 1st year, 3rd sem of college,
+       she has always been with me.
+    </p>
+
+</div>
