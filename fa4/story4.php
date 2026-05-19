faa35fe97dc013a589be4643fcb6d34d3d32214b
AsMagandingan <asmagandingan@gmail.com>
2026-05-19 12:52:29 +0800

    Create story4.php

---
+++ fa4/story4.php
@@ -0,0 +1,14 @@
+<?php require 'header.php'; ?>
+
+<div class="container">
+    <h2>The Honest Farmer</h2>
+
+    <img src="images/story4.jpg">
+
+    <p>
+        A farmer found a bag full of money on the road. Instead of keeping it,
+        he returned it to the owner. Because of his honesty, he was rewarded greatly.
+    </p>
+</div>
+
+<?php include 'footer.php'; ?>
