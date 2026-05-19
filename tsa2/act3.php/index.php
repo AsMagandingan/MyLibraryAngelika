6fd792225c18a693952aaae8c20448d622d7b367
AsMagandingan <asmagandingan@gmail.com>
2026-05-19 19:03:45 +0800

    Create index.php

---
+++ tsa2/act3.php/index.php
@@ -0,0 +1,19 @@
+<?php require('includes/header.php'); ?>
+<?php include('includes/navigation.php'); ?>
+<?php include('includes/hero.php'); ?>
+
+<div class="content">
+    <?php include('includes/objective.php'); ?>
+    <div class="two-col">
+        <?php include('includes/personal.php'); ?>
+        <?php include('includes/education.php'); ?>
+    </div>
+    <?php include('includes/skills.php'); ?>
+    <?php include('includes/certifications.php'); ?>
+    <div class="two-col">
+        <?php include('includes/affiliations.php'); ?>
+        <?php include('includes/experience.php'); ?>
+    </div>
+</div>
+
+<?php require('includes/footer.php'); ?>
